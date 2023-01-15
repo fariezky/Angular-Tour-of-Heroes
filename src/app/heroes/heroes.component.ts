2911e51fcf1f001ced96b33d16acc0da81f96e9c
Fadhil Ariezky <fadhilzentrax@gmail.com>
2023-01-15 22:57:59 +0700

    hero upload

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+  constructor(private heroService: HeroService, private messageService: MessageService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeroes()
+      .subscribe(x => {
+        console.log(x);
+        this.heroes = x;
+      })
+  }
+
+  onSelected(hero: Hero): void{
+    this.messageService.add(`You selected Hero with id of ${hero.id} and name ${hero.name}`)
+    console.log(hero);
+    this.selectedHero = hero;
+  }
+
+  hero: Hero = {
+    id: 1,
+    name: 'dsd'
+  }
+}
